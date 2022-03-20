41aaa550356085aa1d5fd0b30c16043b134ec645
Rotat1On <vvglvv1@gmail.com>
2022-03-20 20:50:22 +0300

    college

--- PZ_15/PZ_15_1.py
+++ PZ_15/PZ_15_1.py
@@ -1,9 +1,9 @@
 import random
-Matrix = [[random.randint(0, 2) for j in range(6)] for i in range(7)]
-print(*Matrix, sep='\n')
-for i in range(7):
+Matrix = [[random.randint(0, 2) for j in range(6)] for i in range(7)]  # создаем матрицу
+print(*Matrix, sep='\n')  # выводим матрицу
+for i in range(7):   # решение задачи
     if i % 2 == 1:
         a = 0
         for j in Matrix[i]:
-            a+= j
-        print(f'Среднее арифметическое нечетной строки {Matrix[i]}: ', a/6)
+            a += j
+        print(f'Среднее арифметическое нечетной строки {Matrix[i]}: ', a/6)  # ответ
--- PZ_15/PZ_15_2.py
+++ PZ_15/PZ_15_2.py
@@ -1,7 +1,13 @@
 import random
-Matrix = [[random.randint(-20,40) for j in range(6)] for i in range(7)]
-print(*Matrix, sep='\n')
+Matrix = [[random.randint(-20,40) for j in range(6)] for i in range(7)]  # создаем матрицу
+print(*Matrix, sep='\n')  # выводим ее
 a = []
-for i in range(7):
-    a.append(max(Matrix[i]))
-print(max(a))
+for i in Matrix:  # решение задачи
+    for j in i:
+        if j % 4 == 0:
+            a.append(j)
+try:
+    print('Ответ таков: ', max(a))  # выводим ответ
+except ValueError:
+    print("Нету числа кратного 4")
+
